19c6bd4b5379d6e8f8e2d20c2ef5064fba47b670
Erne <ernestodainesiperrot@gmail.com>
2020-10-14 12:12:34 -0300

    added skip pop-ups after logging in

--- instagram/comment_bot.py
+++ instagram/comment_bot.py
@@ -16,10 +16,24 @@ class User:
         self.driver = webdriver.Firefox(PATH)
         #self.driver.maximize_window()
 
+    def wait_for_element(self, tag, value):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((tag, value)))
+        except:
+            self.quit_browser()
+
+    def wait_for_clickable(self, tag, value):
+        try:
+            WebDriverWait(self.driver,
+                          10).until(EC.element_to_be_clickable(
+                              (tag, value))).click()
+        except:
+            self.quit_browser()
+
     def load_instagram(self):
         self.driver.get("http://instagram.com")  # Opens instagram on firefox
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.NAME, "username")))
+        self.wait_for_element(By.NAME, "username")
 
     def login(self):
         self.load_instagram()
@@ -28,6 +42,16 @@ class User:
         username_element.send_keys(self.username)
         password_element.send_keys(self.password)
         password_element.send_keys(Keys.RETURN)
+
+    def close_pop_ups(self):
+        self.wait_for_element(By.CLASS_NAME, "cmbtv")
+        self.wait_for_clickable(
+            By.XPATH, "//button[@class='sqdOP yWX7d    y3zKF     ']")
+        self.wait_for_element(By.CLASS_NAME, "mt3GC")
+        self.wait_for_clickable(By.XPATH, "//button[@class='aOOlW   HoLwm ']")
+
+    def got_to_profile(self):
+        self.close_pop_ups()
 
     def quit_browser(self):
         self.driver.quit()
@@ -43,5 +67,7 @@ if __name__ == "__main__":
     username, password = get_user_data()
     user = User(username, password)
     user.login()
-    time.sleep(10)
-    user.quit_browser()
+    user.got_to_profile()
+
+    #time.sleep(10)
+    #user.quit_browser()
