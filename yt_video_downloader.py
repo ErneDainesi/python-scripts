5520d1163e5e710c9011d14014d1687eb90545ae
Erne <ernestodainesiperrot@gmail.com>
2020-10-19 10:30:55 -0300

    refactored

--- yt_video_downloader.py
+++ yt_video_downloader.py
@@ -1,22 +1,26 @@
 import pytube
 import os
 
+
 def get_url():
     return input("url: ")
+
 
 def load_url(url):
     youtube = pytube.YouTube(url)
     video = youtube.streams.first()
     return video
 
+
 def download_video(video):
     save_folder = "/home/erne/" + input("dir: ")
-    salve_folder = save_folder.rstrip("\n")
     while not os.path.exists(save_folder):
-	save_folder = input("[INVALID_INPUT] dir: ")
+        save_folder = input("[INVALID_INPUT] dir: ")
+    save_folder.rstrip("\n")
     print(f'Downloading "{video.title}"...')
     video.download(save_folder)
     print("Done!")
+
 
 url = get_url()
 video = load_url(url)
